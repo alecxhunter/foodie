24253d1b7d919d4cd0fc44424b60e0d6cb70b78c
Alecx Hunter <alecx315@gmail.com>
2020-01-02 15:02:26 -0500

    index style changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppBar, Toolbar, CssBaseline } from '@material-ui/core';
-import { MuiThemeProvider } from '@material-ui/core/styles';
+import { MuiThemeProvider, makeStyles } from '@material-ui/core/styles';
 
 import Recipes from './containers/recipes';
 import theme from './theme';
 import NavBar from './components/navbar';
 
-const App = () => (
-   <MuiThemeProvider theme={theme}>
-      <CssBaseline />
-      <AppBar color="primary" position="static">
-         <Toolbar>
-            <NavBar />
-         </Toolbar>
-      </AppBar>
-      <main style={{overflow: 'hidden', marginRight: theme.spacing(2), marginLeft: theme.spacing(2)}}>
-          <Recipes />
-      </main>
-   </MuiThemeProvider>
-)
+const useStyles = makeStyles(theme => ({
+   margin: {
+      marginRight: theme.spacing(2),
+      marginLeft: theme.spacing(2)
+   }
+}));
+
+function App() {
+   const classes = useStyles();
+   return (
+      <MuiThemeProvider theme={theme}>
+         <CssBaseline />
+         <AppBar color="primary" position="fixed">
+            <Toolbar>
+               <NavBar />
+            </Toolbar>
+         </AppBar>
+         <Toolbar />
+         <main className={classes.margin}>
+            <Recipes />
+         </main>
+      </MuiThemeProvider>
+   )
+}
 
 ReactDOM.render(<App />, document.getElementById("app"));
